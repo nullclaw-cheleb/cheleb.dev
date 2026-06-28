36bb1e661cbb3ac8d582d783cc79ff1b21c5589f
Morgul <nullclaw@cheleb.dev>
2026-06-28 15:32:17 +0200

    Add Three.js WebGL scene with rotating cube

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,6 +1,42 @@
 package example
 
 import org.scalajs.dom
+import threesjs._
+
+@main
+def helloWorld(): Unit = {
+  val canvas = dom.document.createElement("canvas").asInstanceOf[dom.HTMLCanvasElement]
+  canvas.id = "webgl"
+  canvas.style.width = "100%"
+  canvas.style.height = "100%"
+  canvas.style.display = "block"
+  dom.document.body.appendChild(canvas)
+
+  val renderer = WebGLRenderer()
+  renderer.setCanvas(canvas)
+  renderer.setSize(dom.window.innerWidth.toDouble, dom.window.innerHeight.toDouble)
+
+  val scene = Scene()
+
+  val camera = PerspectiveCamera(75, dom.window.innerWidth.toDouble / dom.window.innerHeight.toDouble, 0.1, 1000)
+  camera.position.z = 4
+
+  val geometry = BoxGeometry()
+  val material = MeshBasicMaterial(color = 0x44aa88)
+  val cube = Mesh(geometry, material)
+  scene.add(cube)
+
+  def animate(): Unit = {
+    dom.window.requestAnimationFrame(animate _)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    renderer.render(scene, camera)
+  }
+
+  animate()
+}
+
+import org.scalajs.dom
 
 
 @main
